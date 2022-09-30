4e1b229055d4d998b53b63329ea156cccb9b51a0
edgardjustino <eddiejustino@gmail.com>
2022-09-30 13:18:57 -0300

    Fazendo o código do JS

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const inputTarefa = document.getElementById('tarefa-nova')
+const inputTarefa = document.getElementById("tarefa-nova")
 
-function adicionarTarefa(){
+function adicionarTarefa() {
     const item = document.createElement("li")
 
-    item.textContent = 'Olá'
+    item.innerHTML = `<li>
+                    <span>${inputTarefa.value}</span>
+                    <button class="excluir">x</button>
+                </li>`;
 
-    cons lista = document.querySelector( "#lista")
-    
+    const lista = document.querySelector("#lista")
+    lista.appendChild(item)
+
+    inputTarefa.value = ""
 }
+
+const botaoAdicionar = document.querySelector("#btn-adicionar")
+botaoAdicionar.addEventListener("click", adicionarTarefa)
+
+function limparLista(){
+    const lista = document.querySelector("#lista");
+    lista.innerHTML = ""
+}
+
+const botaoLimpar = document.querySelector(".btn-azul")
+
+botaoLimpar.addEventListener("click", limparLista)
+
